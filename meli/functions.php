8e0cb95a6da3a1c5c74686955d1d7eae556dc6b4
AleBj <alejandromarsico@gmail.com>
2020-09-12 12:45:06 -0300

    functions

--- meli/functions.php
+++ meli/functions.php
@@ -133,8 +133,6 @@
 		});
 	}
 
-	
-
 	add_post_type('novedades', 'Novedades', 'novedad', 'dashicons-welcome-write-blog', array('title', 'editor', 'thumbnail', 'revisions'), array('category', 'post_tag', 'page-category'));
 	add_post_type('notas', 'Notas', 'nota', 'dashicons-welcome-write-blog', array('title', 'editor', 'thumbnail', 'revisions'), array('category', 'post_tag', 'page-category'));
 	add_post_type('webinars', 'Webinars', 'webinar', 'dashicons-format-video', array('title', 'editor', 'thumbnail', 'revisions'), array('category', 'post_tag', 'page-category'));

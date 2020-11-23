764a2559791b69f6271c8d1356f5797bb7fa9933
AleBj <alejandromarsico@gmail.com>
2020-11-23 12:40:33 -0300

    Alertas y webinars

--- meli/single-novedades.php
+++ meli/single-novedades.php
@@ -39,8 +39,6 @@ function slugify($text)
 <main>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-
 	<?php 
 	if( have_rows('alertas_nota') ):
 		$alert = get_field('alertas_nota');

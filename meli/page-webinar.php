524ae6ca2b53993daab64a17b1f7327560d86ce5
AleBj <alejandromarsico@gmail.com>
2021-03-16 14:23:01 -0300

    webinar

--- meli/page-webinar.php
+++ meli/page-webinar.php
@@ -72,7 +72,7 @@ if(isset($_GET['u'])){
                     ),
                     array(
                         'key' => 'fecha_webinar',
-                        'value' => $fechaActual,//date('Y-m-d H:i:s'),
+                        'value' => date('Y-m-d H:i:s', strtotime("- 3 hours")),
                         'compare' => '>=',
                         'type' => 'DATE'
 
